de03b489014a63f319268ab22fe8ec422bb9f540
2467512j <2467512j@student.gla.ac.uk>
2022-01-17 20:36:45 +0000

    created initial working code of food diary, need to add proper fields but this works for now

--- nhs_mobile/src/global_structures.js
+++ nhs_mobile/src/global_structures.js
@@ -1,7 +1,9 @@
 user_struct = {name: "", age: "", height: "", weight: "", blood_type: ""}
-
-food_diary = []
+food_diary_entry = {date: "", meal: "", time: "", food: [], water: "", kcal: "", fat: "", carb: "", sugar: "", protein: ""}
 
 
 diary_dict = {}
 diry_paths = {}
+
+// {item: "", brand: "", amount: ""}
+// ^^^ might need to put this in food_diary_entry

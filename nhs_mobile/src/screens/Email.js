b358878a262cb5dc82d7090da760499e504d9293
Keivan Shooshtarian <2366345s@student.gla.ac.uk>
2022-02-10 03:14:15 +0000

    Created HTMLCONTENT code to be converted from JSON. Related to #47. Currently have issues with htmlcode being accepted by react due to issues with nesting backtickes. Therefore, needs fixing during later commits. Also looks ugly but focusing on functionality rather than styling at the moment

--- nhs_mobile/src/screens/Email.js
+++ nhs_mobile/src/screens/Email.js
@@ -1,5 +1,5 @@
 import * as MailComposer from 'expo-mail-composer';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import {
     View,
     StyleSheet,
@@ -24,34 +24,211 @@ export default function Email({navigation}) {
 
     DropDownPicker.setListMode("SCROLLVIEW");
     const [selected, setSelectedRecipient] = useState("")
+    const [selectedDiary, setSelectedDiary] = useState("")
 
-    const [email_open, setOpen] = useState(false);
-    const [email_value, setValue] = useState(null);
-    const [email, setEmail] = useState([])
+    const [email_open, setEmailOpen] = useState(false);
+    const [email_value, setEmailValue] = useState(null);
+    const [email, setEmail] = useState([]);
 
+    const [diary_open, setDiaryOpen] = useState(false);
+    const [diary_value, setDiaryValue] = useState(null);
+
+    // depends on what diary is actually presented to user NEED TO FIX AFTER MERGE
+    const [diaryItems, setDiaryItems] = useState([
+        {label: 'Blood Pressure', value: 'Blood Pressure'},
+        {label: 'Food Diary', value: 'Food Diary'},
+        {label: 'Glucode Diary', value: 'Glucode Diary'}
+    ]);
+
+    // For DropDownPicker to close a picker when another opened
+    const onEmailOpen = useCallback(() => {
+        setDiaryOpen(false);
+    }, []);
+
+    const onDiaryOpen = useCallback(() => {
+        setEmailOpen(false);
+    }, []);
+
+
+    //Will be changed to not populate this page with lots of sccript information later. Currently optimising for blood pressure diary
+    /*
     const htmlContent = `
     <!DOCTYPE html>
     <html lang="en">
+    
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Pdf Content</title>
+        <title>${selectedDiary}</title>
         <style>
-            body {
-                font-size: 16px;
-                color: rgb(255, 196, 0);
-            }
-            h1 {
-                text-align: center;
+            table, th, tr {
+                border:1px solid black;
             }
         </style>
     </head>
+    
     <body>
-        <h1>Hello, UppLabs!</h1>
+        <h1>Diary: ${selectedDiary}</h1>
+        <h1>NHS Number: TO BE IMPLEMENTED </h1>
+        <h1>Date Generated: <span id="date"></span></h1>
+    
+        <script>
+            n = new Date();
+            y = n.getFullYear();
+            m = n.getMonth() + 1;
+            d = n.getDate();
+            document.getElementById("date").innerHTML = d + "/" + m + "/" + y;
+        </script>
+        
+        <!--Daily Results Table-->
+        <p>Daily Results Table</p>
+        
+        <table style="width:100%">
+            <tr>
+                <th>DATE</th>
+                <th>TIME</th>
+                <th>PERIOD</th>
+                <th>SYSTOLIC</th>
+                <th>DIASTOLIC</th>
+                <th>ARM</th>
+            </tr>
+            
+            <tbody id="dailyResults"></tbody>
+        
+        </table>
+        
+        
+        <br>
+        <!--Average Results Table-->
+        <p>Average Results Table</p>
+        
+        <table style="width:100%">
+            <tr>
+                <th>DATE</th>
+                <th>MORNING AVERAGE SYSTOLIC</th>
+                <th>MORNING AVERAGE DIASTOLIC</th>
+                <th>EVENING AVERAGE SYSTOLIC</th>
+                <th>EVENING AVERAGE DIASTOLIC</th>
+            </tr>
+            
+            <tbody id="averageResults"></tbody>
+        
+        </table>
+    
+        <script>
+            //BLOOD PRESSURE JSON
+            var bloodPressure = [{
+                "date": "15/01/2021",
+                "morning": {"arm": "left",
+                            "sys1": {"bp": 148, "time": "11:00"},
+                            "sys2": {"bp": 117, "time": "11:30"},
+                            "dia1": {"bp": 90, "time": "11:00"},
+                            "dia2": {"bp": 76, "time": "11:30"},
+                            "sys_avg": 132,
+                            "dia_avg": 83},
+                "evening": {"arm": "right",
+                            "sys1": {"bp": 141, "time": "16:00"},
+                            "sys2": {"bp": 110, "time": "16:30"},
+                            "dia1": {"bp": 86, "time": "16:00"},
+                            "dia2": {"bp": 70, "time": "16:30"},
+                            "sys_avg": 126,
+                            "dia_avg": 78}
+            },
+            {
+                "date": "16/01/2021",
+                "morning": {"arm": "right",
+                            "sys1": {"bp": 11248, "time": "11:00"},
+                            "sys2": {"bp": 11327, "time": "11:30"},
+                            "dia1": {"bp": 2130, "time": "11:00"},
+                            "dia2": {"bp": 71236, "time": "11:30"},
+                            "sys_avg": 21412,
+                            "dia_avg": 234},
+                "evening": {"arm": "left",
+                            "sys1": {"bp": 123, "time": "16:00"},
+                            "sys2": {"bp": 134, "time": "16:30"},
+                            "dia1": {"bp": 8232, "time": "16:00"},
+                            "dia2": {"bp": 72442, "time": "16:30"},
+                            "sys_avg": 124124,
+                            "dia_avg": 23}
+            }]
+            
+            // needs slash star when fixed 
+            
+            (((Object.keys(jcontent[0].morning).length)-3)/2)+1              
+            
+            Iterates for amount of rows for sys and dia results. Since the amount of sys and dia results will vary depending on amount user inputs. 
+            3 is taken away since there are three fields that don't relate to the blood pressures (they are time, average sys and average dia). 
+            Next, divided by 2 since there are multiple fields for a result (sys1 and dia1 equate to 2 fields but in reality they only take 1 row). 
+            Addition of 1 is for index through the array easier.
+            
+            // need star slash when fixed
+            
+            // Daily Results Scripting
+            var dailyResults = document.getElementById('dailyResults');
+            
+            for (var day=0; day <bloodPressure.length; day++) {
+                for (var morn=1; morn < (((Object.keys(bloodPressure[day].morning).length)-3)/2)+1; morn++) {
+                    var sys = "sys";
+                      var dia = "dia";
+                      var sysX = sys + morn;
+                      var diaX = dia + morn;
+                    
+                    var row = `<tr>
+                                <td>${bloodPressure[day].date}</td>
+                                <td>${bloodPressure[day].morning[sysX].time}</td>
+                                <td>Morning</td>
+                                <td>${bloodPressure[day].morning[sysX].bp}</td>
+                                <td>${bloodPressure[day].morning[diaX].bp}</td>
+                                <td>${bloodPressure[day].morning.arm}</td>
+                              </tr>`
+                  dailyResults.innerHTML += row
+                }
+                
+                for (var eveng=1; eveng < (((Object.keys(bloodPressure[day].evening).length)-3)/2)+1; eveng++) {
+                    var sys = "sys";
+                      var dia = "dia";
+                      var sysX = sys + eveng;
+                      var diaX = dia + eveng;
+                    
+                    var row = `<tr>
+                                <td>${bloodPressure[day].date}</td>
+                                <td>${bloodPressure[day].evening[sysX].time}</td>
+                                <td>Evening</td>
+                                <td>${bloodPressure[day].evening[sysX].bp}</td>
+                                <td>${bloodPressure[day].evening[diaX].bp}</td>
+                                <td>${bloodPressure[day].evening.arm}</td>
+                                </tr>`
+                  dailyResults.innerHTML += row
+                }
+                
+            }
+            
+            // Average Results Scripting
+            var averageResults = document.getElementById('averageResults');
+            
+            for (var day=0; day <bloodPressure.length; day++) {
+    
+                    var row = `<tr>
+                                <td>${bloodPressure[day].date}</td>
+                                  <td>${bloodPressure[day].morning.sys_avg}</td>
+                                <td>${bloodPressure[day].morning.dia_avg}</td>
+                                <td>${bloodPressure[day].evening.sys_avg}</td>
+                                <td>${bloodPressure[day].evening.dia_avg}</td>
+                              </tr>`
+                  averageResults.innerHTML += row
+            }
+            
+        </script>
+    
     </body>
+    
     </html>
-    `
-/*
+`
+*/
+
+
+    /*
+    // To share the pdf file
     const printToFile = async () => {
         // On iOS/android prints the given html. On web prints the HTML from the current page.
         const { uri } = await Print.printToFileAsync({
@@ -99,24 +276,6 @@ export default function Email({navigation}) {
             htmlContent
         });
         
-
-        /*
-        const { uri } = await FileSystem.downloadAsync(
-            'http://techslides.com/demos/sample-videos/small.mp4',
-            FileSystem.documentDirectory + 'any.mp4'
-          )
-          
-          /*.then(({ uri }) => {
-              console.log('yooo' ,uri)
-          }
-          
-          );*/
-
-        //console.log('this is uri ', uri);
-       // console.log('this is uri ', uri);
-       // console.log('Doc dir has been saved to:', FileSystem.documentDirectory);
-       // console.log('cache dir has been saved to:', FileSystem.cacheDirectory);
-       // console.log(FileSystem.documentDirectory + 'example');
        
         try{
             console.log('this is uri ', uri);
@@ -131,11 +290,38 @@ export default function Email({navigation}) {
         }
     }
 
-    return (
+    return (   
         <SafeAreaView style={styles.body}>
             <ScrollView>
                 <View style={styles.body}>
                     <Header></Header>
+
+                    <Text style={[GlobalStyle.CustomFont,styles.text]}>
+                        Select which diary to send to a doctor from your diary list
+                    </Text>
+                    <DropDownPicker
+                        dropDownDirection="BOTTOM"
+                        style={DropdownStyle.style}
+                        containerStyle={DropdownStyle.containerStyle}
+                        placeholderStyle={DropdownStyle.placeholderStyle}
+                        textStyle={DropdownStyle.textStyle}
+                        labelStyle={DropdownStyle.labelStyle}
+                        listItemContainerStyle={DropdownStyle.itemContainerStyle}
+                        selectedItemLabelStyle={DropdownStyle.selectedItemLabelStyle}
+                        selectedItemContainerStyle={DropdownStyle.selectedItemContainerStyle}
+                        showArrowIcon={true}
+                        showTickIcon={true}
+                        placeholder="Select from diary list!"
+                        open={diary_open}
+                        onOpen={onDiaryOpen}
+                        value={diary_value}
+                        items={diaryItems}
+                        setOpen={setDiaryOpen}
+                        setValue={setDiaryValue}
+                        setItems={setDiaryItems}
+                        onChangeValue={value => setSelectedDiary(value)}
+                    />
+
                     <Text style={[GlobalStyle.CustomFont,styles.text]}>
                         Send and email to a doctor from your doctors list
                     </Text>
@@ -153,10 +339,11 @@ export default function Email({navigation}) {
                         showTickIcon={true}
                         placeholder="Select from doctors list!"
                         open={email_open}
+                        onOpen = {onEmailOpen}
                         value={email_value}
                         items={email}
-                        setOpen={setOpen}
-                        setValue={setValue}
+                        setOpen={setEmailOpen}
+                        setValue={setEmailValue}
                         setItems={setEmail}
                         onChangeValue={value => setSelectedRecipient(value)}
                     />

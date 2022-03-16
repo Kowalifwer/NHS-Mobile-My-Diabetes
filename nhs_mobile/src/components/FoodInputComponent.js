de0c602b7e44f15b2aea16ce784ca278c2ee4343
Artem Grechushkin <artem.grechushkin@mail.ru>
2022-03-16 10:38:52 +0000

    Data fetched by Barcode scanner, now fecthes nutrients STRICTLY per 100g

--- nhs_mobile/src/components/FoodInputComponent.js
+++ nhs_mobile/src/components/FoodInputComponent.js
@@ -16,37 +16,48 @@ import {object_is_empty} from '../global_structures.js'
 // this is the food input which gets added to the page when user clicks the + button
 
 const query_object_for_food_component = (object, key) => {
-    if (object_is_empty(object)) return null
-    if (object[key] == undefined) return null
+    if (object_is_empty(object)) return ""
+    if (object[key] == undefined) return ""
     return object[key].toString()
 }
 
 //define all inputs in here!
-const render_input_components = [
-    {placeholder: "Food Name", is_numeric: false, component_update_key: "name", is_nutrient: false},
-    {placeholder: "Amount (g)", is_numeric: true, component_update_key: "amount", is_nutrient: false},
-    {placeholder: "Protein (per 100g)", is_numeric: true, component_update_key: "protein", is_nutrient: true},
-    {placeholder: "Sugar (per 100g)", is_numeric: true, component_update_key: "sugar", is_nutrient: true},
-    {placeholder: "Fat (per 100g)", is_numeric: true, component_update_key: "fat", is_nutrient: true},
-    {placeholder: "Carbohydrates (per 100g)", is_numeric: true, component_update_key: "carb", is_nutrient: true},
-    {placeholder: "Energy (kcal)", is_numeric: true, component_update_key: "energy", is_nutrient: true},
-] // the KCAL value is now called Energy with kcal as the units
+
 
 const FoodInputComponent = (props) => {
     const {food_input_components_data, setFoodInputComponentsData, id, setBarcodeScannerOpen, barcode_scanner_open} = props
-        
+    const [scanned_data, setScannedData] = useState(null);
+
+    const [render_input_components, set_render_input_components] = useState([
+        {placeholder: "Food Name", is_numeric: false, component_update_key: "name", is_nutrient: false, current_value: ""},
+        {placeholder: "Amount (g)", is_numeric: true, component_update_key: "amount", is_nutrient: false, current_value: ""},
+        {placeholder: "Protein (per 100g)", is_numeric: true, component_update_key: "protein", is_nutrient: true, current_value: ""},
+        {placeholder: "Sugar (per 100g)", is_numeric: true, component_update_key: "sugar", is_nutrient: true, current_value: ""},
+        {placeholder: "Fat (per 100g)", is_numeric: true, component_update_key: "fat", is_nutrient: true, current_value: ""},
+        {placeholder: "Carbohydrates (per 100g)", is_numeric: true, component_update_key: "carb", is_nutrient: true, current_value: ""},
+        {placeholder: "Energy (kcal)", is_numeric: true, component_update_key: "energy", is_nutrient: true, current_value: ""},
+    ]) // the KCAL value is now called Energy with kcal as the units
+
+    useEffect(() => { //If scanned data changes - make sure to update the input fields as well!
+        if (scanned_data) {
+            set_render_input_components(state => (state.map(entry => {
+                return {...entry, current_value: (entry.is_nutrient) ? query_object_for_food_component(scanned_data["nutrients"], entry.component_update_key) : query_object_for_food_component(scanned_data, entry.component_update_key)}
+            })));
+        }
+    }, [scanned_data]);
+
     return (
         <View style={GlobalStyle.BodyGeneral}>
             {(object_is_empty(food_input_components_data[id]["scanned_item_object"])) ? 
-                <Text style={[GlobalStyle.CustomFont, GlobalStyle.Cyan, {marginTop:30}]}>Diary data for food item {id+1}. </Text> : 
-                <Text style={[GlobalStyle.CustomFont, GlobalStyle.Cyan]}>Some data for food item {id+1} has been filled by Barcode scanner! Please double check and make sure to update anything that is not correct!</Text>
+                <Text style={[GlobalStyle.CustomFont, GlobalStyle.Blue, {marginTop:15, marginBottom:25 }]}>Diary data for food item {id+1}. </Text> : 
+                <Text style={[GlobalStyle.CustomFont, GlobalStyle.Blue, {marginTop:15, marginBottom:25 }]}>Some data for food item {id+1} has been filled by Barcode scanner! Please double check and make sure to update anything that is not correct!</Text>
             }
             {/* Render all inputs here! All logic is dealt with, please define components above if neccesary */}
             {render_input_components.map((val, index) => 
                 <SmartTextInput
                     key={index}
+                    value={val.current_value}
                     placeholder={val.placeholder}
-                    defaultValue={(val.is_nutrient) ? query_object_for_food_component(food_input_components_data[id]["scanned_item_object"]["nutrients"], val.component_update_key) : query_object_for_food_component(food_input_components_data[id]["scanned_item_object"], val.component_update_key)}
                     onChangeText={(value) => {
                         setFoodInputComponentsData(state => (state.map(entry => { //updates the food_input_components_data
                             if (entry.index == id) {
@@ -56,13 +67,18 @@ const FoodInputComponent = (props) => {
                                     return {...entry, [val.component_update_key]: value}
                                 }
                             } return entry;
-                        })))
+                        })));
+                        set_render_input_components(state => (state.map(entry => {
+                            if (entry.component_update_key == val.component_update_key) {
+                                return {...entry, current_value: value}
+                            } return entry;
+                        })));
                     }}
                     keyboardType={(val.is_numeric) ? "numeric" : "default"}
                 />
             )}
             
-            {(barcode_scanner_open[0] && barcode_scanner_open[1] == id) && <BarcodeScannerComponent id={id} setFoodInputComponentsData={setFoodInputComponentsData} setBarcodeScannerOpen={setBarcodeScannerOpen} barcode_scanner_open={barcode_scanner_open}/>}
+            {(barcode_scanner_open[0] && barcode_scanner_open[1] == id) && <BarcodeScannerComponent id={id} setFoodInputComponentsData={setFoodInputComponentsData} setBarcodeScannerOpen={setBarcodeScannerOpen} barcode_scanner_open={barcode_scanner_open} setScannedData={setScannedData}/>}
             
             {(!(barcode_scanner_open[0] && barcode_scanner_open[1] == id)) && <CustomButton
                 color="#f96a3e"
